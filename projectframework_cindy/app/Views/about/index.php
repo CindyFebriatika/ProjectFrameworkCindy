e666f8c152aa31630e54bbabc5c0baf73a4097fb
CindyFebriatika <febriantikacindy@gmail.com>
2023-07-02 09:15:22 -1200

    first

---
+++ projectframework_cindy/app/Views/about/index.php
@@ -0,0 +1,30 @@
+<?= $this->extend('layout/layout') ?>
+<?= $this->section('content') ?>
+<div class="row d-flex justify-content-center">
+    <div class="col-md-6">
+        <h1>Tentang Layar Kaca Cindy</h1>
+    </div>
+</div>
+<div class="row d-flex justify-content-center">
+    <div class="col-md-6 " style="text-align: justify;">
+
+        <p>Selamat datang di Layar Kaca Cindy, tempat terbaik untuk menikmati berbagai jenis film dan konten hiburan berkualitas. Kami adalah platform online yang menyediakan akses mudah dan cepat ke berbagai film terbaru, klasik, dan populer dari seluruh dunia. Dengan koleksi film yang beragam dan update terbaru, kami siap memenuhi selera dan kebutuhan hiburan Anda. </p>
+
+        <p>Di Layar Kaca Cindy, kami berkomitmen untuk memberikan pengalaman menonton yang luar biasa bagi pengguna kami. Kami menyajikan tampilan yang user-friendly, navigasi yang intuitif, dan fitur-fitur canggih yang memudahkan Anda dalam menemukan film-film favorit. Dapatkan akses ke katalog film yang lengkap dengan genre yang beragam, mulai dari action, drama, komedi, horor, hingga film-film dokumenter yang informatif.</p>
+
+        <p> Selain itu, kami juga memberikan informasi terkini mengenai sinopsis film, trailer, ulasan, dan rating dari pengguna lain, sehingga Anda dapat membuat keputusan yang tepat sebelum memilih film yang ingin ditonton. Di Layar Kaca Cindy, Anda dapat menikmati film-film favorit Anda di mana saja dan kapan saja melalui perangkat mobile, tablet, atau komputer.</p>
+
+        <p> Kami juga memahami pentingnya keamanan dan kenyamanan saat menikmati konten digital. Oleh karena itu, kami menjaga privasi pengguna dengan cermat dan mengutamakan kebijakan hak cipta serta legalitas dalam menyediakan film-film yang kami tampilkan.</p>
+
+        <p> Layar Kaca Cindy bukan hanya sekadar platform streaming film, tapi juga merupakan komunitas yang memperluas wawasan dan kecintaan terhadap dunia perfilman. Bergabunglah dengan jutaan pengguna lainnya untuk berbagi pengalaman, merekomendasikan film-film favorit, dan terlibat dalam diskusi seru seputar dunia perfilman.</p>
+
+        <p> Dengan misi untuk menyediakan hiburan berkualitas, memberikan pengalaman menonton yang tak terlupakan, dan menjadi sumber inspirasi bagi para pecinta film, Layar Kaca Cindy hadir untuk memenuhi kebutuhan hiburan Anda dengan kepuasan yang tak tertandingi.</p>
+
+        <p> Nikmati pengalaman menonton film yang tak terbatas dan berkualitas tinggi hanya di Layar Kaca Cindy. Segera bergabung dengan komunitas kami dan temukan dunia film yang menyenangkan dan menghibur. Selamat menikmati perjalanan film Anda di Layar Kaca Cindy!</p>
+
+        <p> Terima kasih telah memilih Layar Kaca Cindy sebagai sumber hiburan film terbaik anda </p>
+
+    </div>
+</div>
+
+<?= $this->endSection() ?>
